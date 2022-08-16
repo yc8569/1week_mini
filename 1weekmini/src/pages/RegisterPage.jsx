5a821c5bfaf830db068899da3e0750e4151994fc
yc8569 <109015431+yc8569@users.noreply.github.com>
2022-08-16 20:05:40 +0900

    axios2

--- 1weekmini/src/pages/RegisterPage.jsx
+++ 1weekmini/src/pages/RegisterPage.jsx
@@ -1,6 +1,86 @@
+// import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { registerUser } from "../redux/modules/userAction";
+
+
+// function RegisterPage(props) {
+//   const [Password, setPassword] = useState("");
+//   const [Username, setUsername] = useState("");
+//   const [ConfirmPasword, setConfirmPasword] = useState("");
+  
+//   const dispatch = useDispatch();
+
+
+//   const onNameHandler = (e) => {
+//     setUsername(e.currentTarget.value);
+//   };
+
+//   const onPasswordHanlder = (e) => {
+//     setPassword(e.currentTarget.value);
+//   };
+
+//   const onConfirmPasswordHandler = (e) => {
+//     setConfirmPasword(e.currentTarget.value);
+//   };
+
+//   const onSubmitHandler = (e) => {
+//     e.preventDefault();
+//     if (Password === ConfirmPasword) {
+//       let body = {
+//         Username: Username,
+//         password: Password,
+//       };
+//       dispatch(registerUser(body)).then((res) => {
+//         alert("가입이 정상적으로 완료되었습니다");
+//         props.history.push("/login");
+//       });
+//     } else {
+//       alert("비밀번호가 일치하지 않습니다");
+//     }
+//   };
+  
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         width: "100%",
+//         height: "100vh",
+//       }}>
+//       <form
+//         onSubmit={onSubmitHandler}
+//         style={{ display: "flex", flexDirection: "column" }}>
+
+//         <label>UserName</label>
+//         <input type="text" value={Username} onChange={onNameHandler} />
+
+//         <label>Password</label>
+//         <input type="password" value={Password} onChange={onPasswordHanlder} />
+
+//         <label>ConfirmPasword</label>
+//         <input
+//           type="password"
+//           value={ConfirmPasword}
+//           onChange={onConfirmPasswordHandler}
+//         />
+//         <br />
+//         <button type="submit">회원 가입</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default RegisterPage;
+
+
+//새로운조인
+
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../redux/modules/userAction";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 function RegisterPage(props) {
@@ -9,7 +89,7 @@ function RegisterPage(props) {
   const [ConfirmPasword, setConfirmPasword] = useState("");
   
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
 
   const onNameHandler = (e) => {
     setUsername(e.currentTarget.value);
@@ -27,13 +107,33 @@ function RegisterPage(props) {
     e.preventDefault();
     if (Password === ConfirmPasword) {
       let body = {
-        Username: Username,
+        username: Username,
         password: Password,
       };
-      dispatch(registerUser(body)).then((res) => {
-        alert("가입이 정상적으로 완료되었습니다");
-        props.history.push("/login");
-      });
+     
+     
+      axios
+          .post(
+          "http://3.35.131.44/user/register",
+          
+          {username: Username,
+            password: Password,}
+          
+          )
+          .then((res) => {
+          console.log(res);
+          // props.history.push("/login");
+          // props.navigate.push("/loginpage");
+         
+          })
+          .catch((err) => {
+          console.log(err);
+          alert("회원가입 실패");
+          });
+         
+
+
+
     } else {
       alert("비밀번호가 일치하지 않습니다");
     }
@@ -72,3 +172,7 @@ function RegisterPage(props) {
 }
 
 export default RegisterPage;
+
+
+
+
