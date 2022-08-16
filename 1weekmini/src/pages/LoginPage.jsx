ed38e6fc8f1d26232e2087b0de4f17814e864211
sangjin <sasangjin@gmail.com>
2022-08-16 23:44:07 +0900

    fix login

--- 1weekmini/src/pages/LoginPage.jsx
+++ 1weekmini/src/pages/LoginPage.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../redux/modules/userAction";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function LoginPage(props) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [Username, setUsername] = useState("");
   const [Password, setPassword] = useState("");
+
+  const goSignup = () => {
+    navigate('/register');
+  }
 
   const onUsernameHandler = (e) => {
     setUsername(e.currentTarget.value);
@@ -23,17 +30,35 @@ function LoginPage(props) {
       password: Password,
     };
 
-    dispatch(loginUser(body))
-      .then((res) => {
-        if (res.payload.loginSuccess) {
-          props.history.push("/");
-        } else {
-          alert(res.payload.message);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    axios
+        .post(
+          "http://3.35.131.44/user/login",
+        
+          {
+            username: Username,
+            password: Password,
+          })
+
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("로그인 실패")
+          });
+          
+
+    // dispatch(loginUser(body))
+    //   .then((res) => {
+    //     if (res.payload.loginSuccess) {
+    //       props.history.push("/");
+    //     } else {
+    //       alert(res.payload.message);
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   };
 
   return (
@@ -53,10 +78,20 @@ function LoginPage(props) {
         <label>Password</label>
         <input type="password" value={Password} onChange={onPasswordHanlder} />
         <br />
-        <button type="submit">Login</button>
+        <button type="submit">로그인</button>
+        <div 
+        onClick={goSignup}
+        style={{
+          textAlign: "right",
+          textDecoration : "underline",
+        }}
+        >회원가입</div>
+        <br />
+        
       </form>
     </div>
   );
 }
 
-export default LoginPage;
+export default LoginPage; 
+
